a57c60a307c4f406846457f3e65a9db588bbed7d
Likith Kumar <likithkumarg425@gmail.com>
2026-09-08 16:50:03 +0000

    Monitoring manifests

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,7 +86,6 @@ module "eks" {
   cluster_version = var.cluster_version
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
-  environment     = var.environment
 
   node_groups = var.node_groups
 
